06db93e33e6badbf0a8f1245420712d869d066f2
Kelly Sheddy <jungaka07@gmail.com>
2018-10-12 14:16:29 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,12 +1,17 @@
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
-  pigeon_data.each do |key, value|
-    value.each do |bird_search|
-      if bird_search["gender"] == gender
-        pigeon_list << bird_search["male"]
+   data.each do |key, value|
+    value.each do |key2, value2_array|
+      value2_array.each do |name|
+        if !pigeon_list.include?(name)
+          pigeon_list[name] = {key => [key2.to_s]}
+        elsif pigeon_list[name].include?(key)
+          pigeon_list[name][key] << key2.to_s
+        else
+          pigeon_list[name][key] = [key2.to_s]
+        end
       end
     end
   end
-  pigeon_list      
-    
+  pigeon_list
 end
