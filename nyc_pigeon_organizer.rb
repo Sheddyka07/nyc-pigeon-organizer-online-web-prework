cfe67826388fbb2d6ebcd75baf929cea179ba695
Kelly Sheddy <jungaka07@gmail.com>
2018-10-12 14:11:57 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,8 @@ def nyc_pigeon_organizer(data)
   pigeon_list = {}
   pigeon_data.each do |key, value|
     value.each do |bird_search|
-      if bird_search["lives"] == lives 
+      if bird_search["gender"] == gender
+        
         
     
 end
